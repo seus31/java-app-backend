11a939b01e21226c261e47892330ba769fa9484e
seus31 <us.balut.big.dipper@gmail.com>
2025-01-11 13:05:42 +0900

    initial commit

---
+++ src/main/java/com/example/java_app_backend/JavaAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_app_backend/JavaAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
